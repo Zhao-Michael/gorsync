bdaf30ff2696b5970a128149c8d16113558a50bc
Michael Zhao <chenchenbbk@hotmail.com>
2026-01-07 20:37:32 +0800

    fix the rename failure 'Access is denied'

--- internal/receiver/receiverrenameio_windows.go
+++ internal/receiver/receiverrenameio_windows.go
@@ -3,8 +3,11 @@
 package receiver
 
 import (
+	"crypto/rand"
+	"encoding/base32"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type pendingFile struct {
@@ -31,7 +34,7 @@ func (p *pendingFile) CloseAtomicallyReplace() error {
 	if err := p.f.Close(); err != nil {
 		return err
 	}
-	if err := os.Rename(p.f.Name(), p.fn); err != nil {
+	if err := rename(p.f.Name(), p.fn); err != nil {
 		return err
 	}
 	return nil
@@ -44,4 +47,59 @@ func (p *pendingFile) Cleanup() error {
 		return err
 	}
 	return err
+}
+
+func makeTempName(origname, prefix string) (tempname string, err error) {
+	origname = filepath.Clean(origname)
+	if len(origname) == 0 || origname[len(origname)-1] == filepath.Separator {
+		return "", os.ErrInvalid
+	}
+	// Generate 10 random bytes.
+	// This gives 80 bits of entropy, good enough
+	// for making temporary file name unpredictable.
+	var rnd [10]byte
+	if _, err := rand.Read(rnd[:]); err != nil {
+		return "", err
+	}
+	name := prefix + "-" + strings.ToLower(base32.StdEncoding.EncodeToString(rnd[:])) + ".tmp"
+	return filepath.Join(filepath.Dir(origname), name), nil
+}
+
+func rename(oldname, newname string) error {
+	err := os.Rename(oldname, newname)
+	if err != nil {
+		// If newname exists ("original"), we will try renaming it to a
+		// new temporary name, then renaming oldname to the newname,
+		// and deleting the renamed original. If system crashes between
+		// renaming and deleting, the original file will still be available
+		// under the temporary name, so users can manually recover data.
+		// (No automatic recovery is possible because after crash the
+		// temporary name is not known.)
+		var origtmp string
+		for {
+			origtmp, err = makeTempName(newname, filepath.Base(newname))
+			if err != nil {
+				return err
+			}
+			_, err = os.Stat(origtmp)
+			if err == nil {
+				continue // most likely will never happen
+			}
+			break
+		}
+		err = os.Rename(newname, origtmp)
+		if err != nil {
+			return err
+		}
+		err = os.Rename(oldname, newname)
+		if err != nil {
+			// Rename still fails, try to revert original rename,
+			// ignoring errors.
+			os.Rename(origtmp, newname)
+			return err
+		}
+		// Rename succeeded, now delete original file.
+		os.Remove(origtmp)
+	}
+	return nil
 }
